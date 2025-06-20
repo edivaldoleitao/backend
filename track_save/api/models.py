e160567844fe5768f4ed6cd5ecc4f357325d5e48
PalomaR-Alves <palomaraissa10@gmail.com>
2025-06-19 21:33:36 -0300

    feat: CRUD Products

--- track_save/api/models.py
+++ track_save/api/models.py
@@ -1,6 +1,6 @@
 
 # Create your models here.
-from api.entities.user import User, UserHistory
+from api.entities.user import User
 from api.entities.product import Product, ProductStore, Store
 from api.entities.list import List, ProductList, Favorite, FavoriteCategories
 from api.entities.price import Price
